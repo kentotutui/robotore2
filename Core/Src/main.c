485411e040235603ed0032c3c35fc7597d8e0d1c
kentotutui <tutui1412kidkent@gmail.com>
2024-04-14 16:19:28 +0900

    コメント文削除

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -635,28 +635,13 @@ int main(void)
 
 
 					  //printf("TargetVelocity, CurrentVelocity\r\n");
-					  //printf("Target_X, Target_Y, Target_Theta, Now_X, Now_Y, Now_Theta, Error_X, Error_Y, Errr_Theta, Velocity, Angularvelocity\r\n");
 					  uint16_t size = getDebugLogSize();
 
-					  /*
-					  for(uint16_t i = 0; i < size; i = i+9){
-						 printf("%f, %f, %f, %f, %f, %f, %f, %f, %f\r\n", getDebugLog(i), getDebugLog(i + 1), getDebugLog(i + 2), getDebugLog(i + 3), getDebugLog(i + 4), getDebugLog(i + 5), getDebugLog(i + 6), getDebugLog(i + 7), getDebugLog(i + 8));
-					  }*/
 					  for(uint16_t i = 0; i < size; i = i+13){
 						 printf("%f, %f, %f , %f, %f, %f , %f, %f, %f , %f, %f, %f, %f\r\n", getDebugLog(i), getDebugLog(i + 1), getDebugLog(i + 2), getDebugLog(i + 3), getDebugLog(i + 4), getDebugLog(i + 5), getDebugLog(i + 6), getDebugLog(i + 7), getDebugLog(i + 8), getDebugLog(i + 9), getDebugLog(i + 10), getDebugLog(i + 11), getDebugLog(i + 12));
 					  }
-					  /*
-					  for(uint16_t i = 0; i < size; i = i+3){
-						 printf("%f, %f, %f\r\n", getDebugLog(i), getDebugLog(i + 1), getDebugLog(i + 2));
-					  }*/
 
-					  /*
-					  for(uint16_t i = 0; i < size; i = i+1){
-						 printf("%f\r\n", getDebugLog(i));
-					  }*/
 					  soiya = 0;
-
-					  //printf("9999, 9999\r\n");
 				  }
 
 				  break;
